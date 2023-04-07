8aa517c6533f45361a245e204650ca9c211aad44
tejasXR <tejasshroff24@gmail.com>
2023-04-07 13:49:16 -0400

    Adding Illustrated Nature assets + folder organizaion

---
+++ Assets/App/Scripts/PomodoroUI.cs
@@ -0,0 +1,8 @@
+using TMPro;
+using UnityEngine;
+
+public class PomodoroUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timerText;
+
+}
--- Assets/Scripts/PomodoroUI.cs
+++
@@ -1,8 +0,0 @@
-using TMPro;
-using UnityEngine;
-
-public class PomodoroUI : MonoBehaviour
-{
-    [SerializeField] private TextMeshProUGUI timerText;
-
-}
